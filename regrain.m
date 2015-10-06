a8cbd58ed442533d3c792b131bafe39cb8b25286
François Pitié <fpitie@mee.tcd.ie>
2015-10-06 15:30:32 +0100

    fixed regrain to better match original papers

--- regrain.m
+++ regrain.m
@@ -1,7 +1,10 @@
 %
 %  regraining post-process to match colour of IR and gradient of I0
 %
-%   IRR = degrain(I_original, I_colourgraded);
+%   IRR = degrain(I_original, I_graded, [smoothness]);
+%
+%   the smoothness (default=1, smoothness>=0) sets the fidelity of the 
+%   original gradient field. e.g. smoothness = 0 implies IRR = I_graded.
 %
 %  (c) F. Pitie 2007
 %
@@ -12,14 +15,24 @@
 %  note: this implementation follows a simple top-down approach
 %        with jacobi iterations.
 %
-function IRR = regrain(I0, IR)
+function IRR = regrain(I0, IR, varargin)
+
+numvarargs = length(varargin);
+if numvarargs > 1
+    error('regrain:TooManyInputs', ...
+        'requires at most 1 optional input');
+end
+
+optargs = {1};
+optargs(1:numvarargs) = varargin;
+[smoothness] = optargs{:};
 
 IRR = I0;
-[IRR] = regrain_rec(IRR, I0, IR, [4 4 16 32 64 128 128]);
+[IRR] = regrain_rec(IRR, I0, IR, [4 16 32 64 64 64 ], smoothness, 0);
 
 end
 
-function [IRR] = solve(IRR, I0, IR, nbits)
+function [IRR] = solve(IRR, I0, IR, nbits, smoothness, level)
 
 hres = size(I0,2);
 vres = size(I0,1);
@@ -37,12 +50,14 @@ x2 = 1:hres;
 x3 = [1 1:hres-1];
 x4 = 1:hres;
 
-G0 = blur(I0, 5);
-G0x = (G0(:,[2:end end], :) - G0(:,1:end, :))*255;
-G0y = (G0([2:end end], :, :) - G0(1:end, :, :))*255;
+G0 = I0;
+G0x = (G0(:,[2:end end], :) - G0(:,[1 1:end-1], :));
+G0y = (G0([2:end end], :, :) - G0([1 1:end-1], :, :));
 dI = sqrt(sum(G0x.^2 + G0y.^2, 3));
-phi = 30./(1 + 10*dI);
-psi = min(dI/5, 1);
+
+h = 2^(-level);
+psi = min(256*dI/5, 1);
+phi = 30./(1 + 10*dI/max(smoothness, eps))*h;
 
 phi1 = (phi(y1,x1) + phi(y0, x0))/2;
 phi2 = (phi(y2,x2) + phi(y0, x0))/2;
@@ -62,9 +77,11 @@ for i=1:nbits
     IRR = num./repmat(den + eps, [1 1 K]) .* (1-rho) + rho.*IRR;
 end
 
+figure; imshow(IRR)
+
 end
 
-function [IRR] = regrain_rec(IRR, I0, IR, nbits)
+function [IRR] = regrain_rec(IRR, I0, IR, nbits, smoothness, level)
 
 hres = size(I0,2);
 vres = size(I0,1);
@@ -75,10 +92,10 @@ if (length(nbits) > 1 && hres2 > 20 && vres2 > 20)
     I02 = imresize(I0, [vres2 hres2], 'bilinear');
     IR2 = imresize(IR, [vres2 hres2], 'bilinear');
     IRR2 = imresize(IRR, [vres2 hres2], 'bilinear');
-    [IRR2] = regrain_rec(IRR2, I02, IR2, nbits(2:end));
+    [IRR2] = regrain_rec(IRR2, I02, IR2, nbits(2:end), smoothness, level+1);
     IRR = imresize(IRR2, [vres hres], 'bilinear');
 end
 
-IRR = solve(IRR, I0, IR, nbits(1));
+IRR = solve(IRR, I0, IR, nbits(1), smoothness, level);
 
 end
