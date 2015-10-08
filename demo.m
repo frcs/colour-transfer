50397f021a92856b151984102573f9ce0de5f6de
François Pitié <fpitie@mee.tcd.ie>
2015-10-08 11:59:31 +0100

    added text to demo

--- demo.m
+++ demo.m
@@ -14,7 +14,12 @@
 %
 % send an email to fpitie@mee.tcd.ie if you want more information
 
-fprintf('  colour transfer demo \n');
+fprintf('Colour transfer demo based on:\n');
+fprintf('  [Pitie05a] Pitie et al. N-Dimensional Probability Density Function Transfer and its Application to Colour Transfer. ICCV05.\n');
+fprintf('  [Pitie05b] Pitie et al. Towards Automated Colour Grading. CVMP05.\n');
+fprintf('  [Pitie07a] Pitie et al. Automated colour grading using colour distribution transfer. CVIU. 2007.\n');
+fprintf('  [Pitie07b] Pitie et al. The linear Monge-Kantorovitch linear colour mapping for example-based colour transfer. CVMP07.\n');
+fprintf('  [Pitie08]  Pitie et al. Enhancement of Digital Photographs Using Color Transfer Techniques. Single-Sensor Imaging. 2008.\n');
 
 fprintf('  ... load images\n');
 
@@ -25,7 +30,6 @@ fprintf('  ... MKL Colour Transfer \n');
 
 IR_mkl = colour_transfer_MKL(I0,I1);
 
-
 fprintf('  ... seed the random number generator\n');
 rng(0);
 
@@ -35,8 +39,9 @@ IR_idt = colour_transfer_IDT(I0,I1,10);
 fprintf('  ... regrain post-processing on IDT results \n');
 IR_idt_regrain = regrain(I0,IR_idt);
 
-fprintf('  ... [ok] \n');
+fprintf('  [ok] \n');
 
+% display results
 screensize = get(0,'ScreenSize');
 sz = [576, 1024];
 figure('Position', [ ceil((screensize(3)-sz(2))/2), ceil((screensize(4)-sz(1))/2), sz(2), sz(1)]);
@@ -54,6 +59,5 @@ imshow(IR_idt); title('Result After IDT Colour Transfer [Pitie05a,Pitie05b,Pitie
 
 subplot('Position',[0.6700 0.01 0.3200 .47]); 
 imshow(IR_idt_regrain); title('After IDT and Regrain [Pitie05b,Pitie07a]'); 
-
 
 
